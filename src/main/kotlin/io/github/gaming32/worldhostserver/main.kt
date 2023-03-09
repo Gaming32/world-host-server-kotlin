d8c72d4224fd4525457bbe7344da100d543c3872
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-03-08 18:57:45 -0600

    -a instead of -b
    I want to use -b and -B for the Bedrock equivalent of -j and -J in
    the future.

--- src/main/kotlin/io/github/gaming32/worldhostserver/main.kt
+++ src/main/kotlin/io/github/gaming32/worldhostserver/main.kt
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
     val parser = ArgParser("world-host-server")
 
     val port by parser.option(ArgType.Int, shortName = "p", description = "Port to bind to").default(9646)
-    val baseAddr by parser.option(ArgType.String, shortName = "b", description = "Base address to use for proxy connections")
+    val baseAddr by parser.option(ArgType.String, shortName = "a", description = "Base address to use for proxy connections")
     val inJavaPort by parser.option(ArgType.Int, shortName = "j", description = "Port to use for Java Edition proxy connections").default(25565)
     val exJavaPort by parser.option(ArgType.Int, shortName = "J", description = "External port to use for Java Edition proxy connections")
 
