6b088092162bdade1e819d9ba87480d0e74ad947
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-03-21 07:03:51 -0500

    Change analytics

--- src/main/kotlin/io/github/gaming32/worldhostserver/analytics.kt
+++ src/main/kotlin/io/github/gaming32/worldhostserver/analytics.kt
@@ -23,16 +23,16 @@ fun WorldHostServer.runAnalytics() = GlobalScope.launch {
         delay(config.analyticsTime)
         if (file.length() <= 0) {
             logger.info("Creating new analytics.csv")
-            file.writeText("timestamp,total,${VALID_COUNTRY_CODES.joinToString(",")}\n")
+            file.writeText("timestamp,total,countries\n")
         }
         logger.info("Updating analytics.csv")
         val timestamp = OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
         var total = 0
         val byCountry = VALID_COUNTRY_CODES.associateWithTo(mutableMapOf()) { 0 }
         wsConnections.forEach { connection ->
-            connection.country?.let { byCountry[it] = byCountry.getValue(it) + 1 }
+            connection.country?.let { byCountry[it] = (byCountry[it] ?: 0) + 1 }
             total++
         }
-        file.appendText("$timestamp,$total,${byCountry.values.joinToString(",")}\n")
+        file.appendText("$timestamp,$total,${byCountry.entries.joinToString(";") { "${it.key}:${it.value}" }}\n")
     }
 }
