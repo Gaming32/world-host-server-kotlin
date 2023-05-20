f695260f2f075539e3fffaa296c368928d3f029b
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-05-20 07:44:23 -0500

    Add shutdownTime arg

--- src/main/kotlin/io/github/gaming32/worldhostserver/main.kt
+++ src/main/kotlin/io/github/gaming32/worldhostserver/main.kt
@@ -4,6 +4,8 @@ import io.github.oshai.KotlinLogging
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.default
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
@@ -22,11 +24,30 @@ val COUNTRIES = WorldHostServer::class.java.getResourceAsStream("/countries.json
 fun main(args: Array<String>) {
     val parser = ArgParser("world-host-server")
 
-    val port by parser.option(ArgType.Int, shortName = "p", description = "Port to bind to").default(9646)
-    var baseAddr by parser.option(ArgType.String, shortName = "a", description = "Base address to use for proxy connections")
-    val inJavaPort by parser.option(ArgType.Int, shortName = "j", description = "Port to use for Java Edition proxy connections").default(25565)
-    val exJavaPort by parser.option(ArgType.Int, shortName = "J", description = "External port to use for Java Edition proxy connections")
-    val analyticsTime by parser.option(DurationArgType, description = "Amount of time between analytics syncs").default(10.minutes)
+    val port by parser.option(
+        ArgType.Int, shortName = "p",
+        description = "Port to bind to"
+    ).default(9646)
+    var baseAddr by parser.option(
+        ArgType.String, shortName = "a",
+        description = "Base address to use for proxy connections"
+    )
+    val inJavaPort by parser.option(
+        ArgType.Int, shortName = "j",
+        description = "Port to use for Java Edition proxy connections"
+    ).default(25565)
+    val exJavaPort by parser.option(
+        ArgType.Int, shortName = "J",
+        description = "External port to use for Java Edition proxy connections"
+    )
+    val analyticsTime by parser.option(
+        DurationArgType,
+        description = "Amount of time between analytics syncs"
+    ).default(10.minutes)
+    val shutdownTime by parser.option(
+        DurationArgType,
+        description = "The amount of time before the server automatically shuts down. Useful for restart scripts."
+    )
 
     parser.parse(args)
 
@@ -49,6 +70,16 @@ fun main(args: Array<String>) {
     }
 
     runBlocking {
+        @Suppress("NAME_SHADOWING")
+        val shutdownTime = shutdownTime
+        if (shutdownTime != null) {
+            launch {
+                logger.info("Automatically shutting down after $shutdownTime")
+                delay(shutdownTime)
+                logger.info("Shutting down because shutdownTime was reached")
+                exitProcess(0)
+            }
+        }
         WorldHostServer(WorldHostServer.Config(
             port,
             baseAddr,
